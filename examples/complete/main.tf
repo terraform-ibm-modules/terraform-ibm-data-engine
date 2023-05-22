94aea429525b6bc5d981322e00bd9fd891f68a9b
ocofaigh <ocofaigh@ie.ibm.com>
2023-05-22 13:06:10 +0100

    fix: fix complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -34,6 +34,7 @@ module "data_engine" {
   tags                       = var.resource_tags
   instance_name              = "${var.prefix}-data_engine"
   plan                       = "standard"
+  kms_encryption_enabled     = true
   existing_kms_instance_guid = module.key_protect_all_inclusive.key_protect_guid
   kms_key_crn                = module.key_protect_all_inclusive.keys["sql.${var.prefix}-data-engine"].crn
 }
